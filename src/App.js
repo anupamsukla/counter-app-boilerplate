36f8e64d45419d858b6067b24ccc3d89bd0021a7
anupamsukla <32832216+anupamsukla@users.noreply.github.com>
2022-06-15 00:19:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
-
+import React, { useState } from "react";
 function App() {
+  const [X, setX] = useState(0)
+  const onclick = (value) => {
+    if (X > 0) {
+      setX(X + value)
+    }
+    else if (X == 0 && value == 1) {
+      setX(X + value)
+    }
+  }
+
   return (
     <div className="App">
-      <h2 data-testid="counter-value">X</h2>
-      <button data-testid="counter-decrement-button"></button>
-      <button data-testid="counter-increment-button"></button>
+      <h2 data-testid="counter-value">{X}</h2>
+      <button data-testid="counter-decrement-button" onClick={() => onclick(-1)}>-</button>
+      <button data-testid="counter-increment-button" onClick={() => onclick(1)}>+</button>
     </div>
   );
 }
